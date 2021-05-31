3cb12b7009b74780ee80920ed5c41dd0525a3a8c
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-05-30 23:41:34 -0600

    Fixed displyaing transation post error message

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -34,22 +34,22 @@ exports.addTransaction = async (req, res, next) => {
       data: transaction,
     });
   } catch (error) {
-    console.log(error);
-    // if (error.name === 'ValidationError') {
-    //   const messages = Object.values(error.errors).map(
-    //     (value) => value.message
-    //   );
+    // console.log(error);
+    if (error.name === 'ValidationError') {
+      const messages = Object.values(error.errors).map(
+        (value) => value.message
+      );
 
-    //   return res.status(400).json({
-    //     success: false,
-    //     error: messages,
-    //   });
-    // } else {
-    //   return res.send(500).json({
-    //     success: false,
-    //     error: 'Server Error',
-    //   });
-    // }
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.send(500).json({
+        success: false,
+        error: 'Server Error',
+      });
+    }
   }
 };
 
